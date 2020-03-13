7a7d5dcce9d80f79564e7037032c44a1801f6c68
bartektelec <bartektelec@gmail.com>
2020-03-13 03:11:20 +0100

    added hero banner

--- src/Root.js
+++ src/Root.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from 'components/organisms/Navbar/Navbar';
+import HeroBanner from 'components/organisms/HeroBanner/HeroBanner';
 
-const Root = props => {
+const Root = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch(
+      'https://api.themoviedb.org/3/tv/123?api_key=da4622c209e92f622296706520a36d5f&language=en-US'
+    )
+      .then(response => response.json())
+      .then(response => setData(response))
+      .catch(err => err);
+  }, []);
   return (
     <>
       <Navbar />
+      <HeroBanner data={data} />
     </>
   );
 };
---
+++ src/components/organisms/HeroBanner/HeroBanner.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import PropTypes from 'prop-types';
+import HeroButtons from 'components/molecules/HeroButtons/HeroButtons';
+
+const keyFrameAnim = keyframes`
+0% {
+    background-position: top center;
+}
+
+100% {
+    background-position: bottom center;
+}
+`;
+
+const StyledTitle = styled.h1`
+  color: #fff;
+  font-size: 4.8rem;
+  font-weight: 600;
+`;
+
+const StyledParagraph = styled.p`
+  color: #fff;
+  font-size: 1.6rem;
+  width: 500px;
+`;
+
+const HeroBg = styled.div`
+  padding: 4.8rem;
+  background-size: cover;
+  background-position: center center;
+  background-image: linear-gradient(90deg, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0)),
+    url(${({ bg }) => bg && bg});
+  animation: ${keyFrameAnim} 25s ease infinite alternate;
+`;
+
+const HeroBanner = ({ data }) => {
+  const bgImage = data.backdrop_path
+    ? `https://image.tmdb.org/t/p/original/${data.backdrop_path}`
+    : `https://image.tmdb.org/t/p/original/${data.poster_path}`;
+  return (
+    <HeroBg bg={bgImage}>
+      <StyledTitle>{data.name}</StyledTitle>
+      <StyledParagraph>{data.overview}</StyledParagraph>
+      <HeroButtons />
+      <StyledParagraph>
+        Starring: Dan Castellaneta, Julie Kavner, Nancy Cartwright
+      </StyledParagraph>
+      <StyledParagraph>
+        Genre:
+        {data.genres &&
+          data.genres.map(genre => <span key={genre.id}> {genre.name}</span>)}
+      </StyledParagraph>
+    </HeroBg>
+  );
+};
+
+HeroBanner.propTypes = {
+  data: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+export default HeroBanner;
