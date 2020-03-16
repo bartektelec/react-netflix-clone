93ff360206abfbd6d6a04bdf0bd48e70a58e0f99
bartektelec <bartektelec@gmail.com>
2020-03-16 20:15:01 +0100

    Updates

---
+++ precache-manifest.87753ee313761cabe23d2d666c7587d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5086b37c13b5620d91ca5e4784c7d85",
+    "url": "/react-netflix-clone/index.html"
+  },
+  {
+    "revision": "8517174ebc810f217059",
+    "url": "/react-netflix-clone/static/js/2.c13232e8.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-netflix-clone/static/js/2.c13232e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b533b9ffdf251ed0ea40",
+    "url": "/react-netflix-clone/static/js/main.73250e70.chunk.js"
+  },
+  {
+    "revision": "9224ffe4fc4c67adf230",
+    "url": "/react-netflix-clone/static/js/runtime-main.20be5509.js"
+  },
+  {
+    "revision": "d20b124f937b0403d1710a562360b481",
+    "url": "/react-netflix-clone/static/media/search.d20b124f.svg"
+  }
+]);
